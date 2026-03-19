dd7b32de2d726ad31ecd1eea916f19031b3e554c
gstoltman <gstoltman@gmail.com>
2026-03-19 14:16:28 -0700

    first

---
+++ constants.py
@@ -0,0 +1,18 @@
+{
+    "bills": "https://en.wikipedia.org/wiki/List_of_Buffalo_Bills_seasons",
+    "dolphins": "https://en.wikipedia.org/wiki/List_of_Miami_Dolphins_seasons",
+    "patriots": "https://en.wikipedia.org/wiki/List_of_New_England_Patriots_seasons",
+    "jets": "https://en.wikipedia.org/wiki/List_of_New_York_Jets_seasons",
+    "ravens": "https://en.wikipedia.org/wiki/List_of_Baltimore_Ravens_seasons",
+    "bengals": "https://en.wikipedia.org/wiki/List_of_Cincinnati_Bengals_seasons",
+    "browns": "https://en.wikipedia.org/wiki/List_of_Cleveland_Browns_seasons",
+    "steelers": "https://en.wikipedia.org/wiki/List_of_Pittsburgh_Steelers_seasons",
+    "texans": "https://en.wikipedia.org/wiki/List_of_Houston_Texans_seasons",
+    "colts": "https://en.wikipedia.org/wiki/List_of_Indianapolis_Colts_seasons",
+    "jaguars": "https://en.wikipedia.org/wiki/List_of_Jacksonville_Jaguars_seasons",
+    "titans": "https://en.wikipedia.org/wiki/List_of_Tennessee_Titans_seasons",
+    "broncos": "https://en.wikipedia.org/wiki/List_of_Denver_Broncos_seasons",
+    "chiefs": "https://en.wikipedia.org/wiki/List_of_Kansas_City_Chiefs_seasons",
+    "raiders": "https://en.wikipedia.org/wiki/List_of_Las_Vegas_Raiders_seasons",
+    "chargers": "https://en.wikipedia.org/wiki/List_of_Los_Angeles_Chargers_seasons"
+}
